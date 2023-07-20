e389924b2de3b591b277461d9366458a5470b179
pinal dholariya <97012828+princedholariya@users.noreply.github.com>
2023-07-20 12:18:50 +0530

    Initial commit: Adding Angular project files.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component , ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'te';
+export class AppComponent implements AfterViewInit {
+   @ViewChild('contentDiv') textcontent!:ElementRef;
+   ngAfterViewInit() {
+    console.log(this.textcontent);
+    // this.changeTextColor();
+    // this.changeBackgroundColor();
+  }
+
+  changeTextColor() {
+    
+    
+    if (this.textcontent) {
+      this. textcontent.nativeElement.style.color = 'blue';
+    }
+  }
+
+  changeBackgroundColor() {
+    
+    if (this.textcontent) {
+      this. textcontent.nativeElement.style.backgroundColor = 'yellow';
+    }
+  }
+
+ 
 }
